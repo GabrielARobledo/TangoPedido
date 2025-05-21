3e1e9d97180ed894bd7f617c6193c111bedbbefb
Gabriel A. Robledo <grobledo@axoft.com>
2025-05-21 15:17:42 -0300

    Eliminé lo que no servía.

--- TangoPedido/app/src/main/java/com/axoft/tangopedido/di/RetrofitClient.kt
+++ TangoPedido/app/src/main/java/com/axoft/tangopedido/di/RetrofitClient.kt
@@ -1,7 +1,5 @@
 package com.axoft.tangopedido.di
 
-import com.axoft.tangopedido.data.remote.api.ClienteRequest
-import com.axoft.tangopedido.data.remote.api.LoginClient
 import com.axoft.tangopedido.data.remote.api.PaisClient
 import dagger.Module
 import dagger.Provides
@@ -9,70 +7,23 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Named
 import javax.inject.Singleton
-
-private const val URL_API: String = "https://run.mocky.io/"
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RetrofitClient {
-
     @Provides
     @Singleton
-    @Named("LoginApi")
-    fun providerRetroFit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(URL_API)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun providerLoginClient(@Named("LoginApi") retrofit: Retrofit): LoginClient {
-        return retrofit.create(LoginClient::class.java);
-    }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-object ClienteModule {
-
-    @Provides
-    @Singleton
-    @Named("ClienteApi")
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:17001//")
+            .baseUrl("http://10.0.2.2:17000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideClienteRequest(@Named("ClienteApi") retrofit: Retrofit): ClienteRequest {
-        return retrofit.create(ClienteRequest::class.java)
-    }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-object PaisModule {
-
-    @Provides
-    @Singleton
-    @Named("PaisApi")
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:17001//")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun providePaisRequest(@Named("PaisApi") retrofit: Retrofit): PaisClient {
+    fun providePaisRequest(retrofit: Retrofit): PaisClient {
         return retrofit.create(PaisClient::class.java)
     }
 }
--- TangoPedido/app/src/main/java/com/axoft/tangopedido/presentation/feature/main/MainViewModel.kt
+++ TangoPedido/app/src/main/java/com/axoft/tangopedido/presentation/feature/main/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.axoft.tangopedido.presentation.feature.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.axoft.tangopedido.domain.usecase.LoginUseCase
+import com.axoft.tangopedido.data.remote.dto.PaisResponse
+import com.axoft.tangopedido.domain.usecase.PaisUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,13 +13,31 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
+class MainViewModel @Inject constructor(private val paisUseCase: PaisUseCase) : ViewModel() {
     private val _isLogged = MutableStateFlow(false)
     val isLogged: StateFlow<Boolean> = _isLogged
 
+    private val _data = MutableLiveData<PaisResponse?>()
+    val data: LiveData<PaisResponse?> = _data
+
     init {
+        getData()
+    }
+
+    fun getData() {
         viewModelScope.launch {
-            _isLogged.value = loginUseCase.invoke(id = 123);
+            try {
+                val result = paisUseCase(
+                    process = 932,
+                    id = 1,
+                    token = "C8DBAA66-6845-4B4F-94AC-69AFAA36A88D",
+                    company = 12
+                )
+                _data.value = result
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _data.value = null
+            }
         }
     }
 
